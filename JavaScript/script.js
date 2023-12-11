95b6522c40b176f732f628b7edcb2eb15f88e385
fullstack96 <fullstack.jalc@gmail.com>
2023-12-11 08:25:32 -0300

    ultima configuracion

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -19,3 +19,38 @@ window.onscroll = function(){
 AOS.init({
     duration:1500,
 });
+
+const $days = document.getElementById('days'),
+$hours = document.getElementById('hours'),
+$minutes = document.getElementById('minutes'),
+$seconds = document.getElementById('seconds'),
+$finalMessage = document.querySelector('.final-sms');
+
+//fecha a futuro
+const countdownDate = new Date('Feb 10, 2024 00:00:00').getTime();
+
+let interval = setInterval(function(){
+    // Obtener fecha actual y milisegundos
+    const now = new Date().getTime();
+
+    // Obtener la distancias entre ambas fechas
+    let distance = countdownDate - now;
+
+    // Calculos a dias-horas-minutos-segundos
+    let days = Math.floor(distance / (1000 * 60 * 60 *24));
+    let hours = Math.floor((distance % (1000 *60 *60 *24 )) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 *60 *60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 *60)) / (1000));
+
+    //Escribimos Resultados
+    $days.innerHTML = days;
+    $hours.innerHTML = hours;
+    $minutes.innerHTML = minutes;
+    $seconds.innerHTML = ('0' + seconds).slice(-2);
+
+    //cuando llegue a 0
+    if(distance < 0) {
+        clearInterval(interval);
+        $finalMessage.style.transform = 'traslateY(0)' ;
+    }
+}, 1000)
